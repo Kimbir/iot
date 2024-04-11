230770b50a7a71da4b558ebf027c43edc14acc2c
Kimbir <161114582+Kimbir@users.noreply.github.com>
2024-04-11 22:12:41 +0400

    Update task1.py

--- less1/task1.py
+++ less1/task1.py
@@ -1,1 +1,8 @@
+def find_floor_entrance(room):
+    floors = 8
+    entrance = 4
+    entrance = (room - 1) // (floors) + 1
+    floor = floors - (room - 1) % floors
+    print(floor, entrance)
 room = int(input())
+find_floor_entrance(room)
